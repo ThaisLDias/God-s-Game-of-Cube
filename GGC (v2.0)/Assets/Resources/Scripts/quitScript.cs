e3a1886e46f139958295f9a1c362b98b9be500d7
Thais Dias <dragonssaur@gmail.com>
2015-08-08 17:03:10 -0300

    God's Game of Cube (v2.0)

---
+++ GGC (v2.0)/Assets/Resources/Scripts/quitScript.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class quitScript : MonoBehaviour {
+
+	// Use this for initialization
+	public void OnMouseDown () {
+		Debug.Log ("Quit");
+		Application.Quit ();
+	}
+	
+
+}
