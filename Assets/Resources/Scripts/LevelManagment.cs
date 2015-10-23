b3511aa1f5af7176f39e0e65d64b109277660c9d
Thais Dias <dragonssaur@gmail.com>
2015-10-22 23:48:43 -0200

    v3.5

---
+++ Assets/Resources/Scripts/LevelManagment.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManagment : MonoBehaviour {
+
+	public AudioClip source;
+
+	void Awake() {
+		DontDestroyOnLoad (this.gameObject);
+	}
+
+	public void levelManager (string level) {
+		audio.clip = source;
+		audio.loop = false;
+		audio.Play ();
+		Application.LoadLevel (level);
+	}
+}
--- GGC (v2.0)/Assets/Resources/Scripts/GameController.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class GameController : MonoBehaviour {
-	
-	void Start () {
-		PlayerPrefs.DeleteAll();
-	}
-}
